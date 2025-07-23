cabeede36cf23b2da6f6053ed73ec1d59e93a3c8
marshall1003 <marciel.almeida.ismart@gmail.com>
2025-07-23 00:43:40 -0300

    Delete T1 directory

--- T1/Analisador lexico - ANTLR.py
+++
@@ -1,7 +0,0 @@
-from antlr4 import *
-from LALexer import LALexer
-
-input_stream = FileStream("entrada.txt", encoding="utf-8")
-lexer = LALexer(input_stream)
-for token in lexer.getAllTokens():
-    print(token)
